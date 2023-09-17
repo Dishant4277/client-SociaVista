e9fe3415341cf70cb750de243caed4a2a1159646
Dishant Balotra <117922857+Dishant4277@users.noreply.github.com>
2023-09-17 12:55:48 +0530

    Update index.jsx

--- src/scenes/loginPage/index.jsx
+++ src/scenes/loginPage/index.jsx
@@ -13,7 +13,7 @@ const LoginPage = () => {
         textAlign="center"
       >
         <Typography fontWeight="bold" fontSize="32px" color="primary">
-          Sociopedia
+          SociaVista
         </Typography>
       </Box>
 
